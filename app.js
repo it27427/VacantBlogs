69b2e11c285a6e6d1518c183e887443473595f8a
it27427 <istiak.tushar.it@gmail.com>
2024-01-31 22:16:11 +0600

    customizing-views: creating-layout

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 const path = require('path');
+const favicon = require('serve-favicon');
 
 // app.use('cors');
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.set('view engine', 'ejs');
 
